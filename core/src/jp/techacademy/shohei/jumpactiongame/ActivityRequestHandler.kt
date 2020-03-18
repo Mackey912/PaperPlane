9a3ed607955219b3c38cfc238e9a55a4a3eccb35
Mackey912 <shohei10912@gamil.com>
2020-03-18 18:08:25 +0900

    Initial commit

---
+++ core/src/jp/techacademy/shohei/jumpactiongame/ActivityRequestHandler.kt
@@ -0,0 +1,5 @@
+package jp.techacademy.shohei.jumpactiongame
+
+interface ActivityRequestHandler {
+    fun showAds(show: Boolean)
+}
---
+++ core/src/jp/techacademy/shohei/jumpactiongame/GameObject.kt
@@ -0,0 +1,13 @@
+package jp.techacademy.shohei.jumpactiongame
+
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.math.Vector2
+
+open class GameObject(texture:Texture,secX:Int,srcY:Int,srcWidth:Int,srcHeight:Int)
+    :Sprite(texture,secX,srcY,srcWidth,srcHeight){
+    val velocity:Vector2
+    init{
+        velocity=Vector2()
+    }
+}
